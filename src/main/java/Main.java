8a296aa16dddf181ba81155c4e8e87c5e5bc6f8b
Jay <jieyi840713@gmail.com>
2023-06-29 20:24:05 +0800

    Logest Common Prefix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        RomanToInteger test = new RomanToInteger();
-        System.out.println(test.romanToInt("MCMXCIV"));
+        LongestCommonPrefix test = new LongestCommonPrefix();
+        String[] arg = {"dog","racecar","car"};
+        System.out.println(test.longestCommonPrefix(arg));
     }
 }
